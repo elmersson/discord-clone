dc0f71ca171e20c5f97e485288b195a52d036cd8
RasmusElmersson <rasmus.elmersson@martinservera.se>
2024-01-13 17:48:49 +0100

    feat: create channel

--- components/server/server-header.tsx
+++ components/server/server-header.tsx
@@ -69,7 +69,7 @@ export const ServerHeader = ({ server, role }: ServerHeaderProps) => {
         )}
         {isModerator && (
           <DropdownMenuItem
-            onClick={() => {}}
+            onClick={() => onOpen('createChannel')}
             className='cursor-pointer px-3 py-2 text-sm'
           >
             Create Channel
