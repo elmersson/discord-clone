4c440b69c3d0746c4da11d2b447f713ceda0ba95
elmersson <elmerssonrasmus@gmail.com>
2024-01-21 10:11:44 +0100

    feat: chat messages (#29)

---
+++ components/chat/chat-messages.tsx
@@ -0,0 +1,92 @@
+'use client';
+
+import { Fragment, useRef, ElementRef } from 'react';
+import { Member, Message, Profile } from '@prisma/client';
+import { Loader2, ServerCrash } from 'lucide-react';
+import { ChatWelcome } from './chat-welcome';
+import { useChatQuery } from '@/hooks/use-chat-query';
+import { group } from 'console';
+
+const DATE_FORMAT = 'd MMM yyyy, HH:mm';
+
+type MessageWithMemberWithProfile = Message & {
+  member: Member & {
+    profile: Profile;
+  };
+};
+
+interface ChatMessagesProps {
+  name: string;
+  member: Member;
+  chatId: string;
+  apiUrl: string;
+  socketUrl: string;
+  socketQuery: Record<string, string>;
+  paramKey: 'channelId' | 'conversationId';
+  paramValue: string;
+  type: 'channel' | 'conversation';
+}
+
+export const ChatMessages = ({
+  name,
+  member,
+  chatId,
+  apiUrl,
+  socketUrl,
+  socketQuery,
+  paramKey,
+  paramValue,
+  type,
+}: ChatMessagesProps) => {
+  const queryKey = `chat:${chatId}`;
+  const addKey = `chat:${chatId}:messages`;
+  const updateKey = `chat:${chatId}:messages:update`;
+
+  const chatRef = useRef<ElementRef<'div'>>(null);
+  const bottomRef = useRef<ElementRef<'div'>>(null);
+
+  const { data, fetchNextPage, hasNextPage, isFetchingNextPage, status } =
+    useChatQuery({
+      queryKey,
+      apiUrl,
+      paramKey,
+      paramValue,
+    });
+
+  if (status === 'pending') {
+    return (
+      <div className='flex flex-1 flex-col items-center justify-center'>
+        <Loader2 className='my-4 h-7 w-7 animate-spin text-zinc-500' />
+        <p className='text-xs text-zinc-500 dark:text-zinc-400'>
+          Loading messages...
+        </p>
+      </div>
+    );
+  }
+
+  if (status === 'error') {
+    return (
+      <div className='flex flex-1 flex-col items-center justify-center'>
+        <ServerCrash className='my-4 h-7 w-7 text-zinc-500' />
+        <p className='text-xs text-zinc-500 dark:text-zinc-400'>
+          Something went wrong!
+        </p>
+      </div>
+    );
+  }
+
+  return (
+    <div ref={chatRef} className='flex flex-1 flex-col overflow-y-auto py-4'>
+      <ChatWelcome type={type} name={name} />
+      <div className='mt-auto flex flex-col-reverse'>
+        {data?.pages?.map((group, i) => (
+          <Fragment key={i}>
+            {group.items.map((message: MessageWithMemberWithProfile) => (
+              <div key={message.id}>{message.content}</div>
+            ))}
+          </Fragment>
+        ))}
+      </div>
+    </div>
+  );
+};
